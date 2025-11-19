770c1b9da9b6aa78821042b29974029658c7ac3a
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-11-19 22:24:55 +0330

    Update BigNumber.java

--- Array/BigNumber.java
+++ Array/BigNumber.java
@@ -3,8 +3,25 @@ public class BigNumber {
 
     private byte[] data;
 
-    BigNumber(String number){
+    public BigNumber(String number) throws Exception {
+
+        for (int i = 0; i < number.length(); i++) {
+
+            char temp = number.charAt(i);
+
+            if (!Character.isDigit(temp)) {
+                throw new Exception("Invalid number entered!");
+            }
+
+            this.data[i] = (byte) (temp);
+        }
+
     }
 
+    public BigNumber(byte[] data) {
+
+        this.data = data.clone();
+
+    }
 
 }
