bf0237b034521f398b7509fa5aa376a9206559e6
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-11-22 22:02:38 +0330

    Update PostfixCaculator.py

--- Stack/postfix_caculator/PostfixCaculator.py
+++ Stack/postfix_caculator/PostfixCaculator.py
@@ -8,13 +8,13 @@ def Calculator(s) -> int:
     
     for token in list:
         if token in operators:
-            temp_1 = stack.pop()
             temp_2 = stack.pop()
+            temp_1 = stack.pop()
             
             result = 0
             
-            condition = not (token == '/' & temp_2 == 0)
-            assert condition, "can not divide numbers by zero!"
+            if token == '/':
+                assert temp_2 != 0, "can not divide by zero!"
             
             results = {'+': (temp_1 + temp_2), '-': (temp_1 - temp_2),
                        '*': (temp_1 * temp_2), '/': (temp_1 / temp_2),
