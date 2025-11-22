31d59d1016a7d3efb4a0b69d732f4c0f5828803e
Ramin <166108058+Ramin2005@users.noreply.github.com>
2025-11-22 17:16:11 +0330

    Update BigNumber.java

--- Array/BigNumber.java
+++ Array/BigNumber.java
@@ -4,7 +4,7 @@ public class BigNumber {
     private byte[] data;
 
     public BigNumber(String number) throws Exception {
-        data = new byte[number.length()];
+        this.data = new byte[number.length()];
 
         for (int i = 0; i < number.length(); i++) {
 
@@ -26,6 +26,23 @@ public class BigNumber {
 
         this.data = data.clone();
 
+    }
+
+    public BigNumber(int number){
+        int copy = number;
+        int n = (copy + "").length();
+
+        for(int i = (n - 1); i > -1; i--){
+
+            byte temp = (byte) (copy % 10);
+            
+            this.data[i] = temp;
+
+            copy = (copy - temp) / 10;
+            
+        }
+
+        this.data = new byte[n];
     }
 
     public String toString() {
